efe9bc66432f35ea461da0de2ddcc2f5fd36b29d
Judith Nathanail <a142@judiths-macbook.lan>
2018-12-17 17:03:08 +0000

    Redo 014

--- 014 - Longest Collatz sequence.py
+++ 014 - Longest Collatz sequence.py
@@ -1,21 +1,21 @@
-def collatz(n,t):
-	t += 1
+sbf = {}
+
+def collatz(n):
 	if n == 1:
-		return t
-	if n%2 == 0:
-		n = int(n/2)
-	else:
-		n = int(3*n +1)
-	return collatz(n,t)
+		return 1
+	n = int(3*n +1) if n%2 else int(n/2)
+	if n not in sbf:
+		sbf[n] = collatz(n)
+	return sbf[n] + 1
 
 biggest = 0
 biggest_length = 0
 for i in range(1,1000000):
-    if i % 10000 == 0:
-        print(i)
-    c = collatz(i,0)
-    if c > biggest_length:
-        biggest = i
-        biggest_length = c
-print(biggest, biggest_length)
-        
+	if i % 10000 == 0:
+		print(i)
+	c = collatz(i)
+	if c > biggest_length:
+		biggest = i
+		biggest_length = c
+
+print(biggest)

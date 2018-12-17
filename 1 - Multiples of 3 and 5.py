44be69b457994446d4aca9176d20f241f21b82b5
Judith Nathanail <a142@judiths-macbook.lan>
2018-12-17 16:28:59 +0000

    Refoldered

---
+++ 1 - Multiples of 3 and 5.py
@@ -0,0 +1,2 @@
+p1a = lambda n : sum([i for i in range(1,n) if i%3 == 0 or i%5 == 0])
+print(p1a(1000))
--- to sort/Problem 40 - Champernowne_s constant.py
+++
@@ -1,6 +0,0 @@
-numstring = ""
-counter = 1
-while len(numstring) < 1000:
-    numstring += str(counter)
-    counter += 1
-print(numstring)

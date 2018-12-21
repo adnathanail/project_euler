d74871a6ac8157f9a881a51e9a054e848c3ad75b
Alexander Nathanail <adnathanail@hotmail.co.uk>
2018-12-21 16:35:27 +0000

    Made 10 tractable

--- 001-050/010 - Summation of primes.py
+++ 001-050/010 - Summation of primes.py
@@ -1,9 +1,8 @@
 primes = []
-lp = 0
 for i in range(2,2000000):
   prime = True
   for prime in primes:
-    if prime > i/2:
+    if prime > i ** 0.5:
       break
     else:
       if i % prime == 0:
@@ -11,7 +10,6 @@ for i in range(2,2000000):
         break
   if prime:
     primes.append(i)
-  if len(primes)%1000 == 0 and len(primes) != lp:
-    lp = len(primes)
-    print(len(primes))
+  if i%250000 == 0:
+    print(i)
 print(sum(primes))

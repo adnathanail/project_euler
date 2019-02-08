a230707fc009f8212f64a60894056e04f3e9ec1e
Alexander Nathanail <adnathanail@hotmail.co.uk>
2019-02-08 12:49:00 +0000

    Golfed problem 53

--- 050-100/053 - Combinatoric selections.py
+++ 050-100/053 - Combinatoric selections.py
@@ -1,16 +1,3 @@
-def fac(n):
-  if n == 1 or n == 0:
-    return 1
-  return n * fac(n-1)
-
-def choose(n, r):
-  if n < r or n < 1 or r < 1:
-    return None
-  return int(fac(n)/(fac(r)*fac(n-r)))
-
-k = 0
-for i in range(1,101):
-  for j in range(1,i):
-    if choose(i, j) > 1000000:
-      k += 1
-print(k)
+fac = lambda n: 1 if n in [0,1] else n * fac(n-1)
+choose = lambda n, r: None if n < r or n < 1 or r < 1 else int(fac(n)/(fac(r)*fac(n-r)))
+print(sum([sum([1 if choose(i, j) > 1000000 else 0 for j in range(1,i)]) for i in range(1,101)]))

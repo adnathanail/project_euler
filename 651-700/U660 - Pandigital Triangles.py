d560dcc05d9b32a4efe8e820eebe83922396eb4e
Alexander Nathanail <adnathanail@hotmail.co.uk>
2019-03-13 00:05:25 +0000

    Problem 660 second attempt

--- 651-700/U660 - Pandigital Triangles.py
+++ 651-700/U660 - Pandigital Triangles.py
@@ -19,14 +19,15 @@ def is_base_pandigital(n1, n2, n3, b):
   return sorted(s) == sorted(chars[:b])
 
 # Algorithm to generate integer-sided triangle with 1 angle of 120 degress
-# Side opposite 120 degrees must be bigger than other 2
-# Generate numbers for this side starting from 3, called s1
-# Generate numbers for either of the other sides up to s1, called s2
-# Generate numbers for third side up to s2, called s3
-# This way no duplicate triplets will be generated
-# Use cos rule to check if each triplet gives an angle of 120
-# cosA = (b^2 + c^2 - a^2)/2bc
-# cos(120) = -0.5
+# Generate numbers for side adjacent to 120 degree angle, called s1, up to arbitrary limit
+# Generate numbers for other side adjacent, called s2, up to s1
+# Use cos rule to get other side, if side is integer then yay
+# a^2  = b^2  + c^2  - 2bcCosA
+# s3^2 = s1^2 + s2^2 - 2s1*s2*-0.5
+#      = s1^2 + s2^2 + s1*s2
+
+def is_integer(n):
+  return n%1 == 0
 
 def cosA(a, b, c):
   return (b**2 + c**2 - a**2)/(2 * b * c)
@@ -36,13 +37,14 @@ maxval = sum([i*base**i for i in range(base)])
 print(maxval)
 s1 = s2 = s3 = 1
 while s1 + s2 + s3 < maxval:
-  print(s1)
+  if not(s1%100):
+    print(s1)
   for s2 in range(1, s1):
-    s3 = 1
-    while cosA(s1, s2, s3) < -0.5 and s3 < s2:
-      s3 += 1
-    if cosA(s1, s2, s3) == -0.5 and is_base_pandigital(s1, s2, s3, base):
-      print(base, s1, s2, s3)
+    s3 = (s1**2 + s2**2 + s1*s2)**0.5
+    if is_integer(s3):
+      s3 = int(s3)
+      if is_base_pandigital(s1,s2,s3,base):
+        print(s1,s2,s3)
   s1 += 1
 
 print(cosA(403, 248, 217))

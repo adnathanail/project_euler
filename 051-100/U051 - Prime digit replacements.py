61086b8bd5d5983867476aba2afb516e745c10e3
Alex Nathanail <alex@vectare.co.uk>
2021-02-21 00:41:45 +0000

    U051 correct solution but too slow

--- 051-100/U051 - Prime digit replacements.py
+++ 051-100/U051 - Prime digit replacements.py
@@ -1,60 +1,65 @@
-# import time
-# start = time.time()
+# import math
 
-primes = [2]
-i = 3
-done = False
-while not done:
-  prime = True
-  for prime in primes:
-    if prime > i/2:
-      break
-    else:
-      if i % prime == 0:
-        prime = False
+# def generate_swaps(n):
+#   num_digits = int(math.log10(n))+1
+#   for i in range(num_digits):
+#     pot = 10 ** i
+#     n_truncated_from_left = n % (pot * 10)
+#     n_truncated_from_both_sides = n_truncated_from_left // pot
+#     n_without_ith_digit = n - (n_truncated_from_both_sides * pot)
+#     print(n_without_ith_digit)
+
+# generate_swaps(2567)
+
+NUMS = ["*", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+def generate_templates(longest_template):
+  if longest_template < 1:
+    return []
+  elif longest_template == 1:
+    return NUMS
+  out = []
+  for shorter in generate_templates(longest_template - 1):
+    for num in NUMS:
+      out.append(num + shorter)
+  return out
+
+def generate_nums_from_template(template):
+  out = set()
+  for i in range(10):
+    out.add(template.replace("*", str(i)))
+  return [int(n) for n in out if n[0] != "0"]
+
+def isprime(n):  # TODO speed up
+  if n < 2:
+    return False
+  primes = [True for _ in range(n + 1)]
+
+  p = 2
+  newp = True
+  while newp:
+    for i in range(p*p, n + 1, p):
+      primes[i] = False
+    
+    newp = False
+    for i in range(p + 1, n):
+      if primes[i - 1]:
+        p = i
+        newp = True
         break
-  if prime:
-    primes.append(i)
-    done = len(primes) >= 100
-  i += 1
 
-def isPrime(n):
-  if n > primes[-1]:
-    return None
-  else:
-    return n in primes
+  return primes[n]
 
-
-import itertools
-import re
-
-def permutate(n):
-  o = []
-  for i in itertools.product([0,1],repeat=len(str(n))):
-    # print(i)
-    s = str(n)
-    for j in range(len(i)):
-      if i[j] == 1:
-        s = s[:j] + "x" + s[j+1:]
-    if s == str(n):
-      continue
-    # print(s)
-    l = []
-    for k in range(9):
-      z = int(re.sub("x",str(k),s))
-      if isPrime(z) and len(str(z)) == len(str(n)):
-        l.append(z)
-    o.append(len(l))
-    # print(l)
-    # print([int(re.sub("x",str(k),s)) for k in range(9) if isPrime(int(re.sub("x",str(k),s)))])
-  return max(o)
-
-# print(permutate(13))
-# print([[permutate(q),q] for q in range(100)])
-l = [[permutate(q),q] for q in range(100)]
-print(l)
-print(max(l))
-
-
-# end = time.time()
-# print(end - start)
+if __name__ == "__main__":
+  biggest_i = 0
+  biggest_i_template = ""
+  for template in generate_templates(5):
+    nums = generate_nums_from_template(template)
+    i = 0
+    for n in nums:
+      if isprime(n):
+        i += 1
+    if i > biggest_i:
+      biggest_i = i
+      biggest_i_template = template
+  print(biggest_i, biggest_i_template)

26f8a55a446c085286fd393a77bd09b2cec248a7
Alex Nathanail <alex@vectare.co.uk>
2021-02-21 00:56:34 +0000

    U51 speed up prime check with memoized sieve of eratosthenes

--- 051-100/U051 - Prime digit replacements.py
+++ 051-100/U051 - Prime digit replacements.py
@@ -1,16 +1,3 @@
-# import math
-
-# def generate_swaps(n):
-#   num_digits = int(math.log10(n))+1
-#   for i in range(num_digits):
-#     pot = 10 ** i
-#     n_truncated_from_left = n % (pot * 10)
-#     n_truncated_from_both_sides = n_truncated_from_left // pot
-#     n_without_ith_digit = n - (n_truncated_from_both_sides * pot)
-#     print(n_without_ith_digit)
-
-# generate_swaps(2567)
-
 NUMS = ["*", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 def generate_templates(longest_template):
@@ -30,34 +17,40 @@ def generate_nums_from_template(template):
     out.add(template.replace("*", str(i)))
   return [int(n) for n in out if n[0] != "0"]
 
-def isprime(n):  # TODO speed up
-  if n < 2:
-    return False
-  primes = [True for _ in range(n + 1)]
+class SieveOfEratosthenes:
+  def __init__(self):
+    self.primes = [True, False, True] # Using 1-indexing
+    self.current_max_prime = 2
 
-  p = 2
-  newp = True
-  while newp:
-    for i in range(p*p, n + 1, p):
-      primes[i] = False
-    
-    newp = False
-    for i in range(p + 1, n):
-      if primes[i - 1]:
-        p = i
-        newp = True
-        break
+  def isprime(self, n):
+    if n < 2:
+      return False
+    self.primes += [True for _ in range(n - self.current_max_prime)]
 
-  return primes[n]
+    p = self.current_max_prime
+    newp = True
+    while newp:
+      for i in range(p*p, n + 1, p):
+        self.primes[i] = False
+      
+      newp = False
+      for i in range(p + 1, n):
+        if self.primes[i]:
+          p = i
+          newp = True
+          break
+
+    return self.primes[n]
 
 if __name__ == "__main__":
+  sieve = SieveOfEratosthenes()
   biggest_i = 0
   biggest_i_template = ""
   for template in generate_templates(5):
     nums = generate_nums_from_template(template)
     i = 0
     for n in nums:
-      if isprime(n):
+      if sieve.isprime(n):
         i += 1
     if i > biggest_i:
       biggest_i = i

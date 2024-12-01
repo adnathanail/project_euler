3319b2ede235cbfb43a5bb6077d61bac2b379020
Alex Nathanail <adnathanail@hotmail.co.uk>
2024-12-01 01:02:03 -0400

    51 v2

--- 051-100/51.py
+++ 051-100/51.py
@@ -1,27 +1,13 @@
-# class bcolors:
-#     HEADER = '\033[95m'
-#     OKBLUE = '\033[94m'
-#     OKCYAN = '\033[96m'
-#     OKGREEN = '\033[92m'
-#     WARNING = '\033[93m'
-#     FAIL = '\033[91m'
-#     ENDC = '\033[0m'
-#     BOLD = '\033[1m'
-#     UNDERLINE = '\033[4m'
-
-# for i in range(10):
-#     for j in range(1, 10 + 1):
-#         x = i * 10 + j
-#         print(f"{x}".zfill(2) + " ", end="")
-#     print()
-
-
 def get_numbers_containing_zeroes():
+    """
+    Generate all numbers containing zeroes in order.
+    These act as our "templates" for creating our potential prime sets
+    """
     i = 10
-    yield i
 
     while True:
         istr = str(i)
+        lenistr = len(istr)
         # If we have only 1 0, increment the first non-zero digit starting from the right (smallest place value)
         if istr.count("0") == 1:
             # If we have just 1 0, then it will either be the last value, in which case we increment the second to last value
@@ -30,25 +16,65 @@ def get_numbers_containing_zeroes():
             # Or it will be somewhere else, and we just increment the last value
             else:
                 i += 1
+        # Otherwise if the last digit is zero we increment that
+        elif istr[-1] == "0":
+            i += 1
         # Otherwise we increment the first digit after we find a zero starting from the right (smallest place value)
         else:
-            for k in range(len(istr) - 1, -1, -1):
-                if istr[k] == "0":
+            for k in range(lenistr - 1, -1, -1):
+                if istr[k] != "0":
                     break
             # Convert string index of first zero digit, to the place value of the column
             # e.g. 100,    k = 2, num_to_add = 1
             #      400334, k = 2, num_to_add = 1000
-            num_to_add = 10 ** (len(istr) - k - 1)
+            num_to_add = 10 ** (lenistr - k - 1)
             i += num_to_add
-        yield i
+        yield istr
+
+
+SINGLE_DIGIT_STRINGS = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+def generate_nums_from_template(template):
+    """
+    Given a template like 1003, return the 10 possible replacements:
+    1003, 1113, 1223, 1333, 1443, 1553, 1663, 1773, 1883, 1993
+    """
+    return [int(template)] + [int(template.replace("0", i)) for i in SINGLE_DIGIT_STRINGS]
+
+
+class SieveOfEratosthenes:
+    """
+    Prime checker which doesn't forget its previous results, so it can be efficiently used lots of times
+    """
+    def __init__(self):
+        self.primes = [True, False, True] # Using 1-indexing
+        self.current_max_prime = 2
+
+    def isprime(self, n):
+        if n < 2:
+            return False
+        self.primes += [True for _ in range(n - self.current_max_prime)]
+
+        p = self.current_max_prime
+        newp = True
+        while newp:
+            for i in range(p*p, n + 1, p):
+                self.primes[i] = False
+            
+            newp = False
+            for i in range(p + 1, n):
+                if self.primes[i]:
+                    p = i
+                    newp = True
+                    break
+
+        return self.primes[n]
 
 
 gen = get_numbers_containing_zeroes()
+sieve = SieveOfEratosthenes()
 
-zz = 0
-num_zero_nums = 0
-while num_zero_nums < 183:
-    zz += 1
-    if "0" in str(zz):
-        print(zz, next(gen))
-        num_zero_nums += 1
+
+for i in range(10):
+    template = next(gen)
+    potential_primes = generate_nums_from_template(template)
+    print(min(potential_primes), [p for p in potential_primes if sieve.isprime(p)])
